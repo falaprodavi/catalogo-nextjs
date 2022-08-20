b8903175acda666f0465dc2f818d33874a1742cb
falaprodavi <agencianatural@gmail.com>
2022-08-20 12:24:03 -0300

    Tela de Produtos

---
+++ pages/products.js
@@ -0,0 +1,427 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Head from "next/head";
+
+export default function Produtos({ info }) {
+  return (
+    <>
+      <Head>
+        <title>Produtos</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">PRODUTOS</h1>
+        </div>
+      </header>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full">
+            <div className="lg:w-3/12 lg:mb-0 -mt-9">
+              <img
+                src="https://ebicen.com.br/images/batata.jpg"
+                alt="content"
+              />
+            </div>
+            <p className="lg:w-9/12 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              havent heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Chichen Itza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full">
+            <div className="lg:w-3/12 lg:mb-0 -mt-9">
+              <img
+                src="https://ebicen.com.br/images/batata.jpg"
+                alt="content"
+              />
+            </div>
+            <p className="lg:w-9/12 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              havent heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Great Pyramid of Giza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full">
+            <div className="lg:w-3/12 lg:mb-0 -mt-9">
+              <img
+                src="https://ebicen.com.br/images/batata.jpg"
+                alt="content"
+              />
+            </div>
+            <p className="lg:w-9/12 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              havent heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Chichen Itza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Great Pyramid of Giza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full">
+            <div className="lg:w-3/12 lg:mb-0 -mt-9">
+              <img
+                src="https://ebicen.com.br/images/batata.jpg"
+                alt="content"
+              />
+            </div>
+            <p className="lg:w-9/12 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              havent heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Chichen Itza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+
+    
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+        
+
+          </div>
+        </div>
+      </section>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full">
+            <div className="lg:w-3/12 lg:mb-0 -mt-9">
+              <img
+                src="https://ebicen.com.br/images/batata.jpg"
+                alt="content"
+              />
+            </div>
+            <p className="lg:w-9/12 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              havent heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Chichen Itza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className=" p-6 rounded-lg">
+                <img
+                  className="rounded w-full object-cover object-center mb-6"
+                  src="https://agencianatural.com.br/admin-ebicen/public/storage/produtos/August2022/Xx118u3F97DSXDtE8rwZ.jpg"
+                  alt="content"
+                />
+
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Great Pyramid of Giza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+
+
+
+
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
